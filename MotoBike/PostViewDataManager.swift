4d6d65fb48c9aeea2e2735b78c755b428267ce84
Cherry <n1046702@ntub.edu.tw>
2017-09-23 01:25:22 +0800

    1.0.0.5
    1. 新增 PostViewDataManager

---
+++ MotoBike/PostViewDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  PostViewDataManager.swift
+//  MotoBike
+//
+//  Created by 川口日成 on 2017/9/23.
+//  Copyright © 2017年 Cherry. All rights reserved.
+//
+
+import UIKit
+
+class PostViewDataManager: NSObject {
+
+}
